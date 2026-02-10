83034e4bb15682b62489d4c6893022c3d8e30948
Sergei <ssnefedov97@mail.ru>
2026-02-10 14:15:58 +0300

    Основа проекта

---
+++ src/Apl/GetMoviesByYearHandler.java
@@ -0,0 +1,4 @@
+package Apl;
+
+public class GetMoviesByYearHandler {
+}
---
+++ src/Server/FilmsCollections.java
@@ -0,0 +1,8 @@
+package Server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FilmsCollections {
+    private Map<Integer, Map<Integer,String>> films = new HashMap<>();
+}
